e388bf2450890bc32cb9ac189b0abf87cd0e1358
tulik <tolqinmirsaliyev@gmail.com>
2024-10-13 10:09:16 -0500

    ssss

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ app.use(cors({
     credentials: true,
 }));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://60-maktabsinfimiz2.netlify.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
+  
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'uploads')));
 
